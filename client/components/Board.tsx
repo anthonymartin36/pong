8b93434a7e6a3a80c16ccdbf986b3f628e467bf7
anthony-martin <27912970+anthonymartin36@users.noreply.github.com>
2023-11-03 10:39:26 +1300

    Board components setup

--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -1,7 +1,19 @@
+import PaddleL from './PaddleL'
+import PaddleR from './PaddleR'
+import Ball from './Ball'
+
 function Board() {
   //console.log('Hello World')
 
-  return <div id="Board" className="GameBoard"></div>
+  return (
+    <div id="Board" className="GameBoard">
+      <PaddleL />
+
+      <Ball />
+
+      <PaddleR />
+    </div>
+  )
 }
 
 export default Board
