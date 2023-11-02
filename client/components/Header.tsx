91c932ef0616ad01b50f17792a1d88a5644796da
Blue Pilkinton-Ching <49179161+Blue-Pilkinton-Ching@users.noreply.github.com>
2023-11-03 09:43:22 +1300

    Completed Header

---
+++ client/components/Header.tsx
@@ -0,0 +1,9 @@
+//import data from './Board'
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <h1>{`Score: ${/*data.score*/ 0} | Rounds: ${/*data.rounds*/ 0}`}</h1>
+    </div>
+  )
+}
