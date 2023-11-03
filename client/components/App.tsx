c938eceeca124e8d595060735a661fbca1445196
andrew-dunlop <92898478+SquidRanger27@users.noreply.github.com>
2023-11-03 14:51:29 +1300

    colour changing border

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,5 @@
 import Header from './Header'
 import Board from './Board'
-
 function App() {
   return (
     <div className="layout">
--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -1,11 +1,25 @@
 import PaddleL from './PaddleL'
 import PaddleR from './PaddleR'
 import Ball from './Ball'
+import { useState } from 'react'
 
 function Board() {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, '0')}`
+
+  const [color, setColor] = useState(getRandomColor)
+
+  const handleClick = (event: UIEvent) => [setColor(getRandomColor)]
   //console.log('Hello World')
   return (
-    <div id="Board" className="GameBoard">
+    <div
+      id="Board"
+      className="GameBoard"
+      style={{ color }}
+      onClick={handleClick}
+    >
       <div className="goalL"></div>
       <PaddleL />
 
@@ -18,4 +32,3 @@ function Board() {
 }
 
 export default Board
-
